d207e8fc1678f342bec5a2b6f2fdb342e6598c5a
Eduard Castellano <educastellano08@gmail.com>
2017-06-16 17:48:08 +0200

    Update index.js
    removed a console.log()

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
  
 const express     = require('express');
 const merge       = require('./src/merge').default;
-console.log(merge)
 
 const app = express();
 const bodyParser = require('body-parser')
